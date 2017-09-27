a1a8b16a790e77d225c7a29987e95fe18068afbc
selemtei <selemtei@gmail.com>
2017-09-26 19:30:54 -0700

    initialize LinkedList.js

---
+++ LinkedList.js
@@ -0,0 +1,19 @@
+function LinkedList(){
+    let Node = function(element){
+        this.element = element;
+        this.next = null;
+    };
+
+    let length = 0;
+    let head = null;
+
+    this.append = function(element){};
+    this.insert = function(position, element){};
+    this.removeAt = function(position){};
+    this.remove = function(element){};
+    this.indexOf = function(element){};
+    this.isEmpty = function(){};
+    this.size = function(){};
+    this.toString = function(){};
+    this.print = function(){};
+}
