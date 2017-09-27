f650bd1ebc41213693fca2b74a2f3a58e280e929
selemtei <selemtei@gmail.com>
2017-09-27 15:16:21 -0700

    create doublyLinkedList object

--- LinkedList.js
+++ LinkedList.js
@@ -122,5 +122,77 @@ function DoublyLinkedList(){
     let head = null;
     let tail = null;
 
-    //methods
+    this.insert = function(position, element){
+        if(position >= 0 && position <= length){
+            let node = new Node(element),
+            current = head,
+            previous,
+            index = 0;
+
+            if(position === 0){
+                if(!head){
+                    head = node;
+                    tail = node;
+                }else{
+                    node.next = current;
+                    current.previous = node;
+                    head = node;
+                }
+            }else if(position === length){
+                current = tail;
+                current.next = node;
+                node.previous = current;
+                tail = node;
+            }else{
+                while(index++ < position){
+                    previous = current;
+                    current = current.next;
+                }
+                node.next = current;
+                previous.next = node;
+
+                current.previous = node;
+                node.previous = previous;
+            }
+            length++;
+            return true;
+        }else{
+            return false;
+        }
+    };
+
+    this.removeAt = function(position){
+        if(position > -1 && position < length){
+            let current = head,
+            previous,
+            index = 0;
+
+            if(position === 0){
+                head = current.next;
+
+                if(length === 1){
+                    tail = null;
+                }else{
+                    head.previous = null;
+                }
+            }else if(position === length - 1){
+                current = tail;
+                tail = current.previous;
+                tail.next = null;
+            }else{
+                while(index++ < position){
+                    previous = current;
+                    current = current.next;
+                }
+                previous.next = current.next;
+                current.next.previous = previous;
+            }
+            length--;
+            return current.element;
+        }else{
+            return null;
+        }
+    };
 }
+
+//Circular LinkedList
