0d5110fc7d8a472011ede15fe7ac4b7ecbb6cc89
selemtei <selemtei@gmail.com>
2017-09-27 14:24:52 -0700

    contain all singlyLinkedList methods

--- LinkedList.js
+++ LinkedList.js
@@ -4,16 +4,123 @@ function LinkedList(){
         this.next = null;
     };
 
+    //private variables
     let length = 0;
     let head = null;
+    
+    //adds new item to the end of the list.
+    this.append = function(element){
+        let node = new Node(element), current;
 
-    this.append = function(element){};
-    this.insert = function(position, element){};
-    this.removeAt = function(position){};
-    this.remove = function(element){};
-    this.indexOf = function(element){};
-    this.isEmpty = function(){};
-    this.size = function(){};
-    this.toString = function(){};
-    this.print = function(){};
+        if(head === null){
+            head = node;
+        }else{
+            current = head;
+            while(current.next){
+                current = current.next;
+            }
+            current.next = node;
+        }
+        length++;
+    };
+
+    this.insert = function(position, element){
+        if(position >= 0 && position <= length){
+            let node = new Node(element),
+            current = head;
+            previous,
+            index = 0;
+
+            if(position === 0){
+                node.next = current;
+                head = node;
+            }else{
+                while(index++ < position){
+                    previous = current;
+                    current = current.next;
+                }
+                node.next = current;
+                previous.next = node;
+            }
+            length++;
+            return true;
+        }else {
+            return false;
+        }
+    };
+    
+    this.removeAt = function(position){
+        if(position > -1 && position < length){
+            let current = head, previous, index = 0;
+            //remove first item.
+            if(position === 0){
+                head = current.next;
+            }else{
+                while(index++ < position){
+                    previous = current;
+                    current = current.next;
+                }
+                previous.next = current.next;
+            }
+            length--;
+            return current.element;
+        }else{
+            return null;
+        }
+    };
+
+    this.remove = function(element){
+        let index = this.indexOf(element);
+        return this.removeAt(index);
+    };
+
+    this.indexOf = function(element){
+        let current = head,
+        index = -1;
+        while(current){
+            if(element === current.element){
+                return index;
+            }
+            index++;
+            current = current.next;
+        }
+        return -1;
+    };
+
+    this.isEmpty = function(){
+        return length === 0;
+    };
+
+    this.size = function(){
+        return length;
+    };
+
+    this.toString = function(){
+        let current = head,
+        string = '';
+        while(current){
+            string += current.element + (current.next ? 'n' : '');
+            current = current.next;
+        }
+        return string;
+    };
+
+    this.getHead = function(){
+        return head;
+    }
+}
+
+//Doubly Linked Lists
+function DoublyLinkedList(){
+    let Node = function(element){
+        this.element = element;
+        this.next = null;
+        this.previous = null;
+    };
+
+    let length = 0;
+    let head = null;
+    let tail = null;
+
+    //methods
 }
