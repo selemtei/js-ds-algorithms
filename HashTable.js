868b5e49e66a7c61a77a301453e9c03684658d00
selemtei <selemtei@gmail.com>
2017-10-13 18:03:35 -0700

    create hash, put, get, and remove methods

--- HashTable.js
+++ HashTable.js
@@ -1,3 +1,25 @@
 function HashTable(){
   var table = [];
+
+  var loseHashCode = function(key){
+    var hash = 0;
+    for(var index = 0; index < key.length; index++){
+      hash = hash + key.charCodeAt(index);
+    }
+    return hash % 37;
+  };
+
+  this.put = function(key, value){
+    var position = loseHashCode(key);
+    console.log(position + ' - ' + key);
+    table[position] = value;
+  };
+
+  this.get = function(key){
+    return table[loseHashCode(key)];
+  };
+
+  this.remove() = function(key){
+    table[loseHashCode(key)] = undefined;
+  };
 }
