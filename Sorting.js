bf72557a86b8102ee596710c7ef738985afb5062
selemtei <selemtei@gmail.com>
2017-10-15 09:59:11 -0700

    create mergeSort method

--- Sorting.js
+++ Sorting.js
@@ -55,4 +55,38 @@ function ArrayList(){
       array[j] = temp;
     }
   };
+
+  var merge = function(left, right){
+    var result = [],
+    il = 0,
+    ir = 0;
+    while(il < left.length && ir < right.length){
+      if(left[il] < right[ir]){
+        result.push(left[il++]);
+      }else{
+        result.push(right[ir++]);
+      }
+    }
+
+    while(il < left.length){
+      result.push(left[il++]);
+    }
+
+    while(ir < right.length){
+      result.push(right[ir++]);
+    }
+    
+    return result;
+  };
+
+  this.mergeSort = function(array){
+    var length = array.length;
+    if(length === 1){
+      return array;
+    }
+    var midPostn = Math.floor(length / 2),
+    left = array.slice(0, midPostn),
+    right = array.slice(midPostn, length);
+    return merge(mergeSort(left), mergeSort(right));
+  };
 }
