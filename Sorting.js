4dc05e4417cd7e3ec0e81560f6e960e13855934b
selemtei <selemtei@gmail.com>
2017-10-15 10:25:55 -0700

    create quickSort method

--- Sorting.js
+++ Sorting.js
@@ -75,7 +75,7 @@ function ArrayList(){
     while(ir < right.length){
       result.push(right[ir++]);
     }
-    
+
     return result;
   };
 
@@ -88,5 +88,42 @@ function ArrayList(){
     left = array.slice(0, midPostn),
     right = array.slice(midPostn, length);
     return merge(mergeSort(left), mergeSort(right));
+  };
+
+  var partition = function(array, left, right){
+    var pivot = array[Math.floor((right + left) / 2)],
+    i = left;
+    j = right;
+
+    while(i <= j){
+      while(array[i] < pivot){
+        i++;
+      }
+      while(array[j] > pivot){
+        j--;
+      }
+      if(i <= j){
+        swap(array, i, j);
+        i++;
+        j--;
+      }
+    }
+    return i;
+  };
+
+  var quick = function(array, left, right){
+    var index; 
+    if(array.length > 1){
+      index = partition(array, left, right);
+      if(left < index - 1){
+        quick(array, left, index - 1);
+      }
+      if(index < right){
+        quick(array, index, right);
+      }
+    }
+  };
+  this.quickSort = function(){
+    quick(array, 0, array.length - 1);
   };
 }
