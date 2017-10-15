41a366325d4656cf62d349480f33069848b3fbe7
selemtei <selemtei@gmail.com>
2017-10-15 10:52:05 -0700

    create binarySearch method

--- Searching.js
+++ Searching.js
@@ -6,3 +6,24 @@ this.linearSearch = function(item, array){
   }
   return -1;
 };
+
+//assumes the array is sorted. 
+this.binarySearch = function(array, item){
+  var low = 0,
+  high = array.length - 1,
+  mid, element;
+
+  while(low <= high){
+    mid = Math.floor((low + high) / 2);
+    element = array[mid];
+
+    if(element < item){
+      low = mid + 1;
+    }else if(element > item){
+      high = mid - 1;
+    }else{
+      return mid;
+    }
+  }
+  return -1;
+};
