989a80d127225da24b7d7fbec7a0af52240ee4de
selemtei <selemtei@gmail.com>
2017-10-14 20:00:39 -0700

    create bubble sort method

--- Sorting.js
+++ Sorting.js
@@ -8,4 +8,21 @@ function ArrayList(){
   this.toString = function(){
     return array.join();
   };
+
+  var swap = function(array, index_1, index_2){
+    var aux = array[index_1];
+    array[index_1] = array[index_2];
+    array[index_2] = aux;
+  };
+
+  this.bubbleSort = function(){
+    var length = array.length;
+    for(var index = 0; index < length; index++){
+      for(var j = 0; j < length - 1; j++){
+        if(array[j] > array[j + 1]){
+          swap(array, j, j + 1);
+        }
+      }
+    }
+  };
 }
